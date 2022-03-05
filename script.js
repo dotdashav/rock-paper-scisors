041793097a5e822bcbbdac892053a2f5918a419d
JavierP <jajepelo@gmail.com>
2022-03-05 12:11:08 -0500

    Updating js

--- script.js
+++ script.js
@@ -8,90 +8,27 @@ const feedback = document.querySelector('.feedback');
 
 
 
-
-//r is for rock, p is for paper and s is for scissors
-let options= ['r','p','s'];
-
-function random (a,b){
-    return 0.5 - Math.random();
+function userChoice (option){
+   let playerChoice = option;
+   return playerChoice;
+}
+ 
+function clickSelection () {
+    r.addEventListener('click', ()=> {return userChoice('r');});
+    s.addEventListener('click', ()=>{return userChoice('s');});
+    p.addEventListener('click', ()=>{return userChoice('p');});
 }
 
-function computerplay () {
-    let mixedarray= options.sort(random);
-    let computerchoice = options[0];
-    return computerChoice = computerchoice;
-}    
+const playerElection =clickSelection();
+playerElection();
 
-let cscore = 0
-let pscore = 0
-
-function choicePlayer( ) {
-        r.addEventListener('click',  function (){return playRound ('r', computerplay())});
-        p.addEventListener('click',  function (){return playRound ('p', computerplay())});
-        s.addEventListener('click',  function (){return playRound ('s', computerplay())});
-}
-
-
-
-
-function playRound ( playerchoice , computerchoice){
-    let option = playerchoice + computerchoice; 
-    if (option === 'pr') {
-        pscore += 1;
-        pScoreNumber.textContent = `${pscore}`;
-        feedback.textContent ='You win, paper beats rock';
-    }
-    else if (option === 'sr'){
-        cscore += 1;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent = 'You lose, rock beats scissors';
-    }
-    else if (option === 'rr'){
-        cscore += 1;
-        pscore += 1;
-        pScoreNumber.textContent = `${pscore}`;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent  = 'Tie Game';
-    }
-    else if (option === 'rp'){
-        cscore += 1;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent  = 'You lose, paper beats rock';
-    }
-    else if (option ==='sp'){
-        pscore += 1;  
-        pScoreNumber.textContent = `${pscore}`;
-        feedback.textContent= 'You win, scissors beats paper';
-    }
-    else if (option ==='pp'){
-        cscore += 1;
-        pscore += 1;
-        pScoreNumber.textContent = `${pscore}`;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent  = 'Tie Game'
-    }
-    else if (option === 'ps'){
-        cscore += 1;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent  = 'You lose, scissors beats paper';
-    }
-    else if (option ==='rs'){
-        pscore += 1;
-        pScoreNumber.textContent = `${pscore}`;
-        feedback.textContent  = 'You win, rock beats scissors';
-    }
-    else if (option === 'ss'){
-        cscore += 1;
-        pscore += 1;
-        pScoreNumber.textContent = `${pscore}`;
-        cScoreNumber.textContent = `${cscore}`;
-        feedback.textContent  = 'Tie Game'
-    }   
-}
-
-function fiveRounds (){ 
-    if ( cscore >=5 || pscore>=5 ) {alert('Stop');}
-    else {choicePlayer(), console.log (cscore)};
-}
-
-fiveRounds();
+const posibilities = {
+    rp : 'lose',
+    rs : 'win',
+    rr: 'tie',
+    pp:'tie',
+    ps:'lose',
+    sp:'win',
+    ss:'tie',
+    sr:'lose'
+};
