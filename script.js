40684f19298f2f7acccf1da95c49293919619cc5
JavierP <jajepelo@gmail.com>
2022-03-05 18:23:29 -0500

    finished

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const s = document.getElementById ('s');
 const feedback = document.querySelector('.feedback');
 const  buttons = document.querySelectorAll ('button');
 const finalResult = document.querySelector ('.finalResult');
+let imgPlayer = document.getElementById('player');
+let imgComputer = document.getElementById('computer');
+
 
 let cscore= 0
 let pscore = 0
+let round = 0
 
 function result () {
     if (cscore > pscore){
@@ -20,13 +24,9 @@ function result () {
 }
 
 function clash (game){
-    if (cscore >=4 || pscore>= 4 ) {
-        result ()
-        return
-     }  
-    else if (game === 'rr' || game === 'pp' || game === 'ss' ){
-        cscore += 1
-        pscore += 1
+   if (game === 'rr' || game === 'pp' || game === 'ss' ){
+        cscore += 0
+        pscore += 0
         cScoreNumber.textContent = `${cscore}`
         pScoreNumber.textContent = `${pscore}`
         feedback.textContent = "It's a tied game"
@@ -64,31 +64,73 @@ function clash (game){
  };
 
 
-function play (playerChoice){
+function play (playerMove){
+    let playerChoice =  playerMove;
     let randomNumber = Math.floor((Math.random() * 3));
     let options = ['r','p','s']; 
-    let computerChoice = options[randomNumber]; 
+    let computerChoice = options[randomNumber]
+   function playerSelection (){ 
+        if (playerChoice == 'r'){
+            imgPlayer.src = 'images/rock.png'
+        }
+        else if (playerChoice == 'p'){
+            imgPlayer.src = 'images/paper.png'
+        } 
+        else if (playerChoice == 's'){
+            imgPlayer.src = 'images/scissors.png'
+        };
+    };
+    playerSelection();
+    function computerSelection (){ 
+        if (computerChoice == 'r'){
+            imgComputer.src = 'images/rock.png'
+        }
+        else if (computerChoice == 'p'){
+            imgComputer.src = 'images/paper.png'
+        } 
+        else if (computerChoice == 's'){
+            imgComputer.src = 'images/scissors.png'
+        }
+    };
+    computerSelection();
     let game= playerChoice+computerChoice;
     clash (game);
 }
- 
+
 function classAdded (id){
     id.classList.add ('clicked')}
 
+    function removeClass (e){
+        if (e.propertyName !== 'background-color'){return}
+        this.classList.remove ('clicked')
+    } 
 
+function roundCounter (){
+    round += 1
+    console.log (round);
+};
 
 function clickSelection () {
     r.addEventListener('click', ()=> { 
+        if (round ==5  ) {result (); return }  
         play('r'), 
         classAdded (r);
+        roundCounter(); 
+        r.addEventListener('transitionend', removeClass);
     });
     s.addEventListener('click', ()=>{ 
+        if (round ==5 ) {result (); return }  
         play('s'), 
         classAdded (s);
+        roundCounter ();
+        s.addEventListener('transitionend', removeClass);
     });
     p.addEventListener('click', ()=>{ 
+        if (round ==5 ) {result (); return }  
         play('p'),  
         classAdded (p);
+        roundCounter();
+        p.addEventListener('transitionend', removeClass);
     });
 }
 
