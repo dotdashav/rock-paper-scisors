881162917add0a536752396caefea16967791ca9
JavierP <jajepelo@gmail.com>
2022-03-05 16:08:32 -0500

    Javascript updated

--- script.js
+++ script.js
@@ -4,31 +4,93 @@ const r = document.getElementById('r');
 const p = document.getElementById ('p'); 
 const s = document.getElementById ('s');
 const feedback = document.querySelector('.feedback');
+const  buttons = document.querySelectorAll ('button');
+const finalResult = document.querySelector ('.finalResult');
+
+let cscore= 0
+let pscore = 0
+
+function result () {
+    if (cscore > pscore){
+        finalResult.textContent = 'You lose'
+    } 
+    else { 
+            finalResult.textContent = 'You win'
+        }
+}
+
+function clash (game){
+    if (cscore >=4 || pscore>= 4 ) {
+        result ()
+        return
+     }  
+    else if (game === 'rr' || game === 'pp' || game === 'ss' ){
+        cscore += 1
+        pscore += 1
+        cScoreNumber.textContent = `${cscore}`
+        pScoreNumber.textContent = `${pscore}`
+        feedback.textContent = "It's a tied game"
+    } 
+    else if (game === 'rp'){
+                cscore += 1        
+                cScoreNumber.textContent = `${cscore}`
+                feedback.textContent = 'You lose. Paper beats rock'
+            }
+    else if (game === 'sr'){
+        cscore += 1
+        cScoreNumber.textContent = `${cscore}`
+        feedback.textContent = 'You lose. Rock beats sicssors'
+    }
+    else if (game === 'ps'){
+        cscore += 1
+        cScoreNumber.textContent = `${cscore}`
+        feedback.textContent = 'You lose. Scissors beats paper'
+    }
+    else if (game === 'pr'){
+        pscore += 1
+        pScoreNumber.textContent = `${pscore}`
+        feedback.textContent = 'You win. Paper beats rock'
+    } 
+    else if (game === 'rs'){
+        pscore += 1
+        pScoreNumber.textContent = `${pscore}`
+        feedback.textContent = 'You win. Rock beats sicssors'
+    }            
+    else if (game === 'sp'){
+        pscore += 1
+        pScoreNumber.textContent = `${pscore}`
+        feedback.textContent = 'You win. Scissors beats paper'
+    }
+ };
 
 
-
-
-function userChoice (option){
-   let playerChoice = option;
-   return playerChoice;
+function play (playerChoice){
+    let randomNumber = Math.floor((Math.random() * 3));
+    let options = ['r','p','s']; 
+    let computerChoice = options[randomNumber]; 
+    let game= playerChoice+computerChoice;
+    clash (game);
 }
  
+function classAdded (id){
+    id.classList.add ('clicked')}
+
+
+
 function clickSelection () {
-    r.addEventListener('click', ()=> {return userChoice('r');});
-    s.addEventListener('click', ()=>{return userChoice('s');});
-    p.addEventListener('click', ()=>{return userChoice('p');});
+    r.addEventListener('click', ()=> { 
+        play('r'), 
+        classAdded (r);
+    });
+    s.addEventListener('click', ()=>{ 
+        play('s'), 
+        classAdded (s);
+    });
+    p.addEventListener('click', ()=>{ 
+        play('p'),  
+        classAdded (p);
+    });
 }
 
-const playerElection =clickSelection();
-playerElection();
 
-const posibilities = {
-    rp : 'lose',
-    rs : 'win',
-    rr: 'tie',
-    pp:'tie',
-    ps:'lose',
-    sp:'win',
-    ss:'tie',
-    sr:'lose'
-};
+clickSelection();
